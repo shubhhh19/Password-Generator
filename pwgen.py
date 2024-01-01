6d9e4c46c8dcaf388d76d5930d89c3d598562a10
shubhhh19 <ssoni7735@conestogac.on.ca>
2024-01-01 08:23:54 -0500

    User input and shuffling of char

--- pwgen.py
+++ pwgen.py
@@ -3,3 +3,10 @@ import random
 
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
+def gen_pass():
+    pass_length = int(input("Enter password length: "))
+
+    random.shuffle(characters)
+    password = []
+
+    
