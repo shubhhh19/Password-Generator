ee7b5736bba2952ab68457bb0ab9b2f25294c54c
shubhhh19 <ssoni7735@conestogac.on.ca>
2024-01-01 09:08:50 -0500

    Password generate and user prompts

--- pwgen.py
+++ pwgen.py
@@ -3,10 +3,25 @@ import random
 
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
-def gen_pass():
-    pass_length = int(input("Enter password length: "))
+def gen_password():
+    password_length = int(input("Enter password length: "))
 
     random.shuffle(characters)
     password = []
 
-    
+    for x in range(password_length):
+        password.append(random.choice(characters))
+        
+    random.shuffle(password)
+    password = "".join(password)
+    print(password)
+
+option = input("Do you want to generate password? (Yes/No):" + "\n")
+if option == "Yes":
+    gen_password()
+elif option == "No":
+    print("Program ended")
+    quit()
+else: 
+    print("Program exited")
+    quit()
